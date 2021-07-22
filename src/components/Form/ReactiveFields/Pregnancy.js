8e2e47e7d4d4d9a9883476de1c43670cc67355d4
Mohd Imran <imrancrazy200@gmail.com>
2021-07-22 10:04:40 +0530

    formik setup complete

---
+++ src/components/Form/ReactiveFields/Pregnancy.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useFormikContext } from "formik";
+
+import TextField from "../TextField";
+
+const Pregnancy = () => {
+    const { 
+        values: {
+            gender,
+        },
+        touched,
+        setFieldValue
+    } = useFormikContext();
+
+
+    useEffect(() => {
+        if (gender === "Female") {
+            setFieldValue('pregnancy', 'APPLICABLE')
+        } else if (gender === "Male" || gender === "Others") {
+            setFieldValue('pregnancy', 'NOT APPLICABLE')
+        }
+    }, [gender, setFieldValue, touched.gender])
+
+    return (
+        <TextField name="pregnancy" label="" />
+    )
+}
+
+export default Pregnancy;
